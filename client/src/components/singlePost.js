95dfefe6359b08c94212cf0585e74f1cafd534f5
Gr8ness21 <pmleechapman@gmail.com>
2019-06-04 13:46:12 -0400

    completed single post component

--- client/src/components/singlePost.js
+++ client/src/components/singlePost.js
@@ -1,115 +1,115 @@
-// import React, { Component } from "react";
-// import { Redirect, Link } from 'react-router-dom';
-// import axios from "axios";
+import React, { Component } from "react";
+import { Redirect, Link } from 'react-router-dom';
+import axios from "axios";
 
-// class SingleCity extends Component {
-//     state = {
-//         city: {
-//             name: '',
-//             description: ''
-//         },
-//         resInfo: {
-//             city: {
-//                 _id: '',
-//                 name: '',
-//                 description: ''
-//             },
-//             parks: []
-//         },
-//         redirectToHome: false,
-//         isEditFormDisplayed: false
-//     }
-// getcity=()=>{
-//     axios.get(`/api/cities/${this.props.match.params.id}`).then(res => {
-//         console.log(res.data)
-//         this.setState({ resInfo: res.data })
-//     })
-// }
+class SinglePost extends Component {
+    state = {
+        post: {
+            title: '',
+            info: ''
+        },
+        resInfo: {
+            post: {
+                _id: '',
+                title: '',
+                info: ''
+            },
+            comments: []
+        },
+        redirectToHome: false,
+        isEditFormDisplayed: false
+    }
+getpost=()=>{
+    axios.get(`/api/posts/${this.props.match.params.id}`).then(res => {
+        console.log(res.data)
+        this.setState({ resInfo: res.data })
+    })
+}
 
-//     componentDidMount = () => {
-//         this.getcity()
+    componentDidMount = () => {
+        this.getpost()
        
-//     }
+    }
 
-//     deleteCity = () => {
-//         axios.delete(`/api/cities/${this.props.match.params.id}`).then(res => {
-//             this.setState({ redirectToHome: true })
-//         })
-//     }
+    deletePost = () => {
+        axios.delete(`/api/posts/${this.props.match.params.id}`).then(res => {
+            this.setState({ redirectToHome: true })
+        })
+    }
 
-//     toggleEditForm = () => {
-//         this.setState((state, props) => {
-//             return { isEditFormDisplayed: !state.isEditFormDisplayed }
-//         })
-//     }
+    toggleEditForm = () => {
+        this.setState((state, props) => {
+            return { isEditFormDisplayed: !state.isEditFormDisplayed }
+        })
+    }
 
-//     handleChange = (e) => {
-//         const cloneCity = { ...this.state.city }
-//         cloneCity[e.target.name] = e.target.value
-//         this.setState({ city: cloneCity })
-//     }
+    handleChange = (e) => {
+        const clonePost = { ...this.state.post }
+        clonePost[e.target.title] = e.target.value
+        this.setState({ post: clonePost })
+    }
 
-//     updateCity = (e) => {
-//         e.preventDefault()
-//         axios
-//             .put(`/api/cities/${this.props.match.params.id}`, {
-//                 name: this.state.city.name,
-//                 description: this.state.city.description
-//             })
-//             .then(res => {
-//                 this.setState({ city: res.data, isEditFormDisplayed: false })
-//             })
-//             this.getcity()
-//     }
+    updateCity = (e) => {
+        e.preventDefault()
+        axios
+            .put(`/api/posts/${this.props.match.params.id}`, {
+                name: this.state.post.title,
+                description: this.state.post.info
+            })
+            .then(res => {
+                this.setState({ post: res.data, isEditFormDisplayed: false })
+            })
+            this.getpost()
+    }
 
-//     render() {
-//         if (this.state.redirectToHome) {
-//             return (<Redirect to="/cities" />)
-//         }
+    render() {
+        if (this.state.redirectToHome) {
+            return (<Redirect to="/posts" />)
+        }
 
-//         return (
-//             <div className="singleCity">
-//                 <Link to="/cities">Back to Cities</Link>
-//                 <h1>{this.state.resInfo.city.name}</h1>
-//                 <p>{this.state.resInfo.city.description}</p>
-//                 <button onClick={this.toggleEditForm}>Edit</button>
-//                 {
-//                     this.state.isEditFormDisplayed
-//                         ? <form onSubmit={this.updateCity}>
-//                             <div>
-//                                 <label htmlFor="name">Name</label>
-//                                 <input
-//                                     id="name"
-//                                     type="text"
-//                                     name="name"
-//                                     onChange={this.handleChange}
-//                                     value={this.state.city.name}
-//                                 />
-//                             </div>
-//                             <div>
-//                                 <label htmlFor="description">Description</label>
-//                                 <textarea
-//                                     id="description"
-//                                     name="description"
-//                                     onChange={this.handleChange}
-//                                     value={this.state.city.description}
-//                                 />
-//                             </div>
-//                             <button>Update</button>
-//                         </form>
-//                         : <div>
-//                             <div>
-//                                 Name: {this.state.city.name}
-//                             </div>
-//                             <div>
-//                                 Description: {this.state.city.description}
-//                             </div>
-//                             <button onClick={this.deleteCity}>Delete</button>
-//                         </div>
-//                 }
-//             </div>
-//         );
-//     }
-// }
+        return (
+            <div className="singlePost">
+                <Link to="/posts">Back to Posts</Link>
+                <h1>{this.state.resInfo.post.title}</h1>
+                <p>{this.state.resInfo.post.info}</p>
+                <button onClick={this.toggleEditForm}>Edit</button>
+                {
+                    this.state.isEditFormDisplayed
+                        ? <form onSubmit={this.updatePost}>
+                            <div>
+                                <label htmlFor="title">Title</label>
+                                <input
+                                    id="title"
+                                    type="text"
+                                    name="title"
+                                    onChange={this.handleChange}
+                                    value={this.state.post.title}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="info">Info</label>
+                                <textarea
+                                    id="info"
+                                    name="info"
+                                    onChange={this.handleChange}
+                                    value={this.state.post.info}
+                                />
+                            </div>
+                            <button>Update</button>
+                        </form>
+                        : <div>
+                            <div>
+                                Name: {this.state.post.title}
+                            </div>
+                            <div>
+                                Info: {this.state.post.info}
+                            </div>
+                            <button onClick={this.deletePost}>Delete</button>
+                        </div>
+                }
+            </div>
+        );
+    }
+}
 
-// export default SingleCity;
+export default SinglePost;
